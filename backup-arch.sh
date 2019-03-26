e58f3c4fa2b83a609d077524d3bba31c8608ce99
Anton-Latukha <anton.latukha@gmail.com>
2019-03-26 08:26:38 +0200

    rm most scripts

--- backup-arch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/dash
-
-# Save list of installed packages
-pacman -Qqe > pkglist-"$(date -I)".txt
-
-# Save pacman database
-tar --create --auto-compress --file pacman_database-"$(date -I)".tar.xz /var/lib/pacman/local/
